9f7cf03fb131c9e232d87265fb9027d43ff77d29
lam-ngo <lam.ngo@pexpay.com>
2023-04-24 09:42:06 +0700

    chore: update favicon

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,5 +1,3 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export default function hello(req: NextApiRequest, res: NextApiResponse) {
